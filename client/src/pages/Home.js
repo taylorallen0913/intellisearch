541f61f31f038d07204e43a68374cc11243bbe38
taylorallen0913 <taylorallen0913@gmail.com>
2020-03-29 05:29:37 -0700

    kai is fixing stuff

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,11 +1,21 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import TextyAnim from "rc-texty";
 
 export default class Home extends Component {
-    render() {
-        return (
-            <div className="container">
-                <h1 className="title">Intellisearch</h1>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className="container">
+          <div className="title">
+        <TextyAnim
+          type="mask-top"
+          onEnd={type => {
+            console.log(type);
+          }}
+        >
+          Intellisearch
+        </TextyAnim>
+        </div>
+      </div>
+    );
+  }
 }
--- client/src/pages/Results.js
+++ client/src/pages/Results.js
@@ -1,11 +1,29 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import listReactFiles from 'list-react-files'
+
+const ImageGallery = require("react-image-gallery");
+const path = require("path");
 
 export default class Results extends Component {
-    render() {
-        return (
-            <div className="container">
-                
-            </div>
-        )
-    }
+  constructor() {
+    super();
+    this.state = {
+      images: [],
+      imagesLoaded: false
+    };
+  }
+
+  componentDidMount() {
+    this.getImages();
+  }
+
+  getImages = async () => {
+    const basePath = "../../public/results/";
+    listReactFiles(basePath).then(files => console.log(files))
+    this.setState({ imagesLoaded: true });
+  };
+
+  render() {
+    return <div className="container"></div>;
+  }
 }
